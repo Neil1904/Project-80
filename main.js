4ac73053cc52d3ad0403ef26045d1402bc55e6a5
Neil1904 <75314458+Neil1904@users.noreply.github.com>
2021-02-18 19:00:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-function getParagraph1(){
+function getParagraph1() {
     var inputs = [];
-    for (i = 0; i <= 6; i++) {
-        inputs.push(document.getElementById("para1_input_box" + i).value);
-
+    for (var i = 1; i <= 6; i++) {
+        inputs.push(document.getElementById("div1_input_box_" + i).value);
     }
-    inputs.join(". ");
-    document.getElementById("showParagraph1").innerHTML=inputs.join(". ");
+    document.getElementById("showParagraph1").innerHTML = inputs.join(". ");
+}
+
+function getParagraph2() {
+    var inputs = [];
+    for (var i = 1; i <= 6; i++) {
+        inputs.push(document.getElementById("div2_input_box_" + i).value);
+    }
+    document.getElementById("showParagraph2").innerHTML = inputs.join(". ");
 }
